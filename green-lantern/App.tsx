d90b197de2eb473295eb794acd0d7e7124c20302
Marilise Morona <marymorona@gmail.com>
2023-10-18 17:36:49 -0300

    Add library Torch and Shake

--- green-lantern/App.tsx
+++ green-lantern/App.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Alert } from 'react-native';
+import Torch from 'react-native-torch';
 import symbolOn from './assets/pictures/symbol_on.png';
 import symbolOff from './assets/pictures/symbol_off.png';
 
@@ -13,6 +14,21 @@ export default function App() {
     })
   }
 
+  // Apresenta o alerta antes do componente
+  //useEffect(()=>{
+    //Alert.alert('Montou o componente')
+  //})
+
+  // Apresentar o alerta depois do componente
+  //useEffect(()=>{
+    //return() =>  Alert.alert('Desmontou o componente')
+  //})
+
+  // Observa o componente e em sua mudança de estado apresenta o Alerta
+  useEffect(()=>{
+    Torch.switchState(isActive);
+    //Alert.alert('Atualizou o componente ' + isActive)
+  }, [isActive])
 
   return (
     <View style={isActive ? styles.containerOn : styles.containerOff}>
